f0465f6e0880764fd48700c71fcea400e93f9919
minh0206 <minh020698@gmail.com>
2025-10-07 15:42:41 +0700

    feat(security-lib): Create auto-configuration class

---
+++ security-jwt-lib/src/main/java/com/fooddelivery/securitylib/config/SecurityLibAutoConfiguration.java
@@ -0,0 +1,22 @@
+package com.fooddelivery.securitylib.config;
+
+import com.fooddelivery.securitylib.filter.JwtAuthenticationFilter;
+import com.fooddelivery.securitylib.service.JwtService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication // This configuration will only be active in a web application context
+public class SecurityLibAutoConfiguration {
+
+    @Bean
+    public JwtService jwtUtil() {
+        return new JwtService();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(JwtService jwtService) {
+        return new JwtAuthenticationFilter(jwtService);
+    }
+}
