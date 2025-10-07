376e3a8f5ebaf6251f183bfb27eb6495ad88ac40
minh0206 <minh020698@gmail.com>
2025-10-07 15:41:49 +0700

    feat(security-lib): Create a generic JWT authentication filter

---
+++ security-jwt-lib/src/main/java/com/fooddelivery/securitylib/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.fooddelivery.securitylib.filter;
+
+import com.fooddelivery.securitylib.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String jwt = authHeader.substring(7);
+        if (SecurityContextHolder.getContext().getAuthentication() == null && jwtService.validateToken(jwt)) {
+            String username = jwtService.extractUsername(jwt);
+            Long userId = jwtService.extractUserId(jwt);
+
+            // We can use the user ID as the principal's name if we want
+            UserDetails userDetails = new User(userId.toString(), "", new ArrayList<>());
+
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities());
+
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
